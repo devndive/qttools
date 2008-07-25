5b798b41e7d4f8c1281e76a6f42d828a132e3c44
yann.duval.82 <yann.duval.82@f9e2dc70-0052-0410-9b99-1d260748578e>
2008-07-25 14:13:25 +0000

    dynamic modules are working now

--- trunk/PFG/definitions.cpp
+++ trunk/PFG/definitions.cpp
@@ -6,11 +6,14 @@ namespace PFG
 
 void addToStringList(stringList &list, const std::string &toAdd)
 {
-	stringList::iterator result = std::find(list.begin(), list.end(), toAdd);
-
-	if( result == list.end() )
+	if( toAdd.size() > 0 )
 	{
-		list.push_back(toAdd);
+		stringList::iterator result = std::find(list.begin(), list.end(), toAdd);
+
+		if( result == list.end() )
+		{
+			list.push_back(toAdd);
+		}
 	}
 }
 
